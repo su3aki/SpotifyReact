47a9552f46e67c1863f7eacae48341f1107cc017
su3aki <su3aki.08@gmail.com>
2021-02-24 19:40:45 +0900

    0224SearchjsStill

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -4,37 +4,28 @@ import axios from 'axios'
 const Search = (props) => {
   //const [trackInfo, setTrackInfo] = useState("")
   //アクセストークン取得
-  const [searchResult, setSearchResult] = useState({
-    artistName: "",
-    artistId: "",
-    trackPreviewURL: "",
-    trackImg: "",
-    trackName: "",
-  })
+  const [searchResult, setSearchResult] = useState([])
   useEffect(() => {
 //曲名単語検索
-      axios(`https://api.spotify.com/v1/search?q=${props.wordFormData}&type=track&market=US&limit=10&offset=5`, {
-        method: "GET",
-        headers: { Authorization: "Bearer " + props.token },
+    axios(`https://api.spotify.com/v1/search?query=${props.wordFormData}&type=track&market=US&limit=10`, {
+      method: "GET",
+      headers: { Authorization: "Bearer " + props.token },
+    })
+      .then((trackContentsResponse) => {
+        setSearchResult(
+          trackContentsResponse.data.tracks.items.album
+        )
+        console.log(trackContentsResponse)
       })
-        .then((trackContentsReaponse) => {
-          setSearchResult({
-            artistName: trackContentsReaponse.data.artists[0].name,
-            artistId: trackContentsReaponse.data.artists[0].id,
-            trackPreviewURL: trackContentsReaponse.data.preview_url,
-            trackImg: trackContentsReaponse.data.album.images[0].url,
-            trackName: trackContentsReaponse.data.name,
-          })
-          .catch((err) => {
-        console.log("err:", err);
+      .catch((err) => {
+        console.log("err:", err)
       })
-        })
-  }, [props.wordFormData,props.token])
-    return (
-      <div>
-        <p>RESULT:{searchResult.trackPreviewURL}</p>
-      </div>
-    )
-
+  }, [props.wordFormData, props.token]
+  )
+  return (
+    <div>
+      <p>RESULT:{searchResult}</p>
+    </div>
+  )
 }
 export default Search;
