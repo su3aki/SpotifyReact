96c432db23ee3677f557dfd2c2c292150cf0304c
su3aki <su3aki.08@gmail.com>
2021-03-28 04:20:52 +0900

    ProductionBuildAdd

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+process.env.NODE_ENV !== "development" && (console.log = () => {});
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
