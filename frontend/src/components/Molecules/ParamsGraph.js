1ab5592347f38281bb1f0681cf93066e686fcded
su3aki <su3aki.08@gmail.com>
2021-03-31 02:27:05 +0900

    bpmReady

--- frontend/src/components/Molecules/ParamsGraph.js
+++ frontend/src/components/Molecules/ParamsGraph.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import CountUp from 'react-countup'
 import { Radar } from 'react-chartjs-2'
 import Grid from '@material-ui/core/Grid'
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import { color } from '@material-ui/system'
+
 
 const ParamsGraph = React.memo((props) => {
   const ShrinkedPopularity = props.FirstPopularity * 0.01
@@ -63,27 +68,61 @@ const ParamsGraph = React.memo((props) => {
       }
     }
   }
+  const useStyles = makeStyles((theme) => ({
+    bpm: {
+      color: '#DF0869',
+      fontSize: '1.5rem',
+    },
+    bpmStyle: {
+      display: "inline-flex",
+      justifyContent: 'center'
+    }
+  }))
+  const classes = useStyles()
   return (
-    <div>
-      <Grid container direction="row">
-        {(props.FirstArtwork).length !== 0
-          &&<Grid item xs={6} sm={2}>
-          <img src={props.FirstArtwork}
-          width="200" height="200"/>
+  <>
+  <Radar data={data}
+    options={myOptions}
+    width={600}
+        height={600} />
+
+  <Grid container direction="row">
+    {(props.FirstTempo) !== undefined
+      &&(props.FirstTempo).length !== 0
+          && <Grid item xs={6} className={classes.bpmStyle}>
+          <Typography className={classes.bpm}>
+          BPM
+          </Typography>
+          <Typography variant="h3" style={{ color: "#1db954" }}>
+            <CountUp end={Math.trunc(props.FirstTempo)} />
+          </Typography>
         </Grid>
-        }
-        {(props.ReArtwork).length !==0
-          && <Grid item xs={6} sm= {2}>
-          <img src={props.ReArtwork}
-          width="200" height="200"/>
-      </Grid>
-        }
-      </Grid>
-        <Radar data={data}
-          options={myOptions}
-          width={600}
-          height={600} />
-    </div>
+    }
+    {(props.ReTempo) !== undefined
+      &&(props.ReTempo).length !== 0
+        && <Grid item xs={6} className={classes.bpmStyle}>
+          <Typography className={classes.bpm}>
+          BPM
+          </Typography>
+          <Typography variant="h3">
+            <CountUp end={Math.trunc(props.ReTempo)}/>
+          </Typography>
+        </Grid>
+    }
+  </Grid>
+      {/* {(props.FirstArtwork).length !== 0
+        &&<Grid item xs={6} sm={2}>
+        <img src={props.FirstArtwork}
+        width="200" height="200"/>
+        </Grid>
+      } */}
+      {/* {(props.ReArtwork).length !==0
+        && <Grid item xs={6} sm= {2}>
+        <img src={props.ReArtwork}
+        width="200" height="200"/>
+        </Grid>
+      } */}
+  </>
   )
 })
 export default ParamsGraph;
